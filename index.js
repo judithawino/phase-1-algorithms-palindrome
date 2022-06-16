e25f26471f6143616a370fac83d0ead25718bf65
Judith Awino Omondi <judith.awino.omondi@gmail.com>
2022-06-16 23:52:58 +0300

    commit tree 25f70ee29263f5bbc82ffbb53216e5de883adfd2

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // using for loop, iterate from the beginning to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to the correponding letter from the end
+    const j = word.length - 1 - i;
+    // if any letters don't match, return false
+    if (word[i] !== word[j]) return false;
+  }
+
+  // return true
+  return true;
 }
+
 
 /* 
   Add your pseudocode here
+
+
 */
 
 /*
